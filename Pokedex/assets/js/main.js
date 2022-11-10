8be006f94181c0b2a3ae3d0a737a662736d4f761
Bruno.Lafayette <brunolafayette@rocketmail.com>
2022-11-10 11:43:57 -0300

    Organizando propriedades da API

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,9 +1,33 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonToHtml (pokemon){
+    return `
+              <li class="pokemon">
+                    <span class="numero">#001</span>
+                    <span class="nomePokemon">${pokemon.name}</span>
+                    
+                    <div class="detalhe">
+                        <ol class="tipos">
+                            <li class="tipo">raio</li>
+                            <li class="tipo">ar</li>
+                        </ol>
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg" alt="${pokemon.name}">
 
-fetch(url)
-    .then((response) => response.json())
-    .then((responseBody) => console.log(responseBody))
+                    </div>
+                </li>
+
+    
+             `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+apiPokemon.getPokemons().then((pokemons) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+    //     for (let i = 0; i < 10; i++){
+    //         let pokemon = pokemons[i]
+    //         console.log(pokemon.name)
+    //         pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+    //     }
+    })
     .catch((error) => console.error(error))
