eef5ce79ec4f20667b2386c044d6946dbeb84c05
Bruno.Lafayette <brunolafayette@rocketmail.com>
2022-11-15 17:57:04 -0300

    Adicionado cores no fundo da pagina

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,16 +1,15 @@
 
 function convertPokemonToHtml (pokemon){
     return `
-              <li class="pokemon">
-                    <span class="numero">#001</span>
+              <li class="pokemon ${pokemon.type}">
+                    <span class="numero">#${pokemon.number}</span>
                     <span class="nomePokemon">${pokemon.name}</span>
                     
                     <div class="detalhe">
                         <ol class="tipos">
-                            <li class="tipo">raio</li>
-                            <li class="tipo">ar</li>
+                            ${pokemon.types.map((type) => `<li class="tipo  ${pokemon.type}">${type}</li>`).join('')}
                         </ol>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg" alt="${pokemon.name}">
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
 
                     </div>
                 </li>
@@ -30,4 +29,4 @@ apiPokemon.getPokemons().then((pokemons) => {
     //         pokemonList.innerHTML += convertPokemonToHtml(pokemon)
     //     }
     })
-    .catch((error) => console.error(error))
+   .catch((error) => console.error(error))
