d4563edee8dc2061bd581ae8890be2713c2cfde8
Bruno.Lafayette <brunolafayette@rocketmail.com>
2022-11-15 20:05:46 -0300

    Adicionando botao para carregar paginas

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,7 +1,13 @@
+const pokemonList = document.getElementById('pokemonList')
+const btnCarregarMais = document.getElementById('btnLoad')
+const limit = 20
+let offset = 0
 
-function convertPokemonToHtml (pokemon){
-    return `
-              <li class="pokemon ${pokemon.type}">
+
+function carregarPokemons (offset, limit){
+    apiPokemon.getPokemons(offset, limit).then((pokemons) => {
+        const newHTML = pokemons.map((pokemon) => `
+                <li class="pokemon ${pokemon.type}">
                     <span class="numero">#${pokemon.number}</span>
                     <span class="nomePokemon">${pokemon.name}</span>
                     
@@ -10,23 +16,22 @@ function convertPokemonToHtml (pokemon){
                             ${pokemon.types.map((type) => `<li class="tipo  ${pokemon.type}">${type}</li>`).join('')}
                         </ol>
                         <img src="${pokemon.photo}" alt="${pokemon.name}">
-
+    
                     </div>
                 </li>
-
+            `
+        
+        ).join('')
     
-             `
+        pokemonList.innerHTML += newHTML
+        
+        })
+       .catch((error) => console.error(error))
 }
 
-const pokemonList = document.getElementById('pokemonList')
+carregarPokemons(offset, limit)
 
-
-apiPokemon.getPokemons().then((pokemons) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
-    //     for (let i = 0; i < 10; i++){
-    //         let pokemon = pokemons[i]
-    //         console.log(pokemon.name)
-    //         pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-    //     }
-    })
-   .catch((error) => console.error(error))
+btnCarregarMais.addEventListener('click', () => {
+    offset += limit
+    carregarPokemons(offset, limit)
+})
